28ea830ca8550fcde275331ebd895202f84c2c12
NizarRestu <nizarrestuaji18@gmail.com>
2024-03-27 15:45:59 +0700

    update

--- src/views/admin/notifikasi_excelcom/notifikasi_7/Notifikasi7Excelcom.js
+++ src/views/admin/notifikasi_excelcom/notifikasi_7/Notifikasi7Excelcom.js
@@ -4,13 +4,14 @@ import "datatables.net";
 import "./../../../../assets/styles/datatables.css";
 import axios from "axios";
 import SidebarAdmin from "../../../../component/SidebarAdmin";
+import { NOTIFIKASI_7_EXCELCOM,  GET_BARANG_TRANSAKSI_JUAL_EXCELCOM } from "../../../../utils/BaseUrl";
 import { Breadcrumbs, IconButton, Typography } from "@material-tailwind/react";
-import { NOTIFIKASI_7_EXCELCOM } from "../../../../utils/BaseUrl";
 import { CheckIcon, PhoneIcon, PrinterIcon } from "@heroicons/react/24/outline";
 
 function Notifikasi7Excelcom() {
   const tableRef = useRef(null);
   const [notifikasis, setNotifikasi] = useState([]);
+  const [barang, setBarang] = useState([]);
 
   const initializeDataTable = () => {
     if ($.fn.DataTable.isDataTable(tableRef.current)) {
@@ -31,12 +32,43 @@ function Notifikasi7Excelcom() {
     }
   };
 
+  // Fetch additional data for each notification, in this case barang information
+  const barangTransaksi = async (transactionId) => {
+    try {
+      const response = await axios.get(
+        `${GET_BARANG_TRANSAKSI_JUAL_EXCELCOM}?id_transaksi=${transactionId}`,
+        {
+          headers: { "auth-tgh": `jwt ${localStorage.getItem("token")}` },
+        }
+      );
+      return response.data.data;
+    } catch (error) {
+      console.log("get all", error);
+      return [];
+    }
+  };
+
+  useEffect(() => {
+    const fetchBarangTransaksi = async () => {
+      const barangList = await Promise.all(
+        notifikasis.map(async (laporan) => {
+          const barangData = await barangTransaksi(laporan.idTransaksi);
+          return barangData;
+        })
+      );
+      setBarang(barangList);
+    };
+
+    fetchBarangTransaksi();
+  }, [notifikasis]);
+
   useEffect(() => {
     getAll();
   }, []);
 
   useEffect(() => {
     if (notifikasis && notifikasis.length > 0) {
+      // Initialize DataTable only when notifikasis data is available
       initializeDataTable();
     }
   }, [notifikasis]);
@@ -79,22 +111,30 @@ function Notifikasi7Excelcom() {
                   <th className="py-2 px-3 font-semibold">No Faktur</th>
                   <th className="py-2 px-3 font-semibold">Nama Customer</th>
                   <th className="py-2 px-3 font-semibold">Nama Salesman</th>
-                  <th className="py-2 px-3 font-semibold">Nama Barang</th>
+                  <th className="py-2 px-3 font-semibold">Barcode Barang</th>
                   <th className="py-2 px-3 font-semibold">Aksi</th>
                 </tr>
               </thead>
               <tbody>
                 {notifikasis.length > 0 ? (
-                  notifikasis.map((penjualan, index) => (
+                  notifikasis.map((penjualan, index) => {
+                    const barangLaporan = barang[index] || [];
+                    return(
                     <tr key={index}>
                       <td className="w-[4%]">{index + 1}</td>
                       <td className="py-2 px-3">{penjualan.created_date}</td>
                       <td className="w-[15%] py-2 px-3">
                         {penjualan.noFaktur}
                       </td>
-                      <td className="py-2 px-3">{penjualan.namaCustomer}</td>
-                      <td className="py-2 px-3">{penjualan.namaSalesman}</td>
-                      <td className="py-2 px-3">{penjualan.namaSalesman}</td>
+                      <td className="py-2 px-3">{penjualan.customer.nama_customer}</td>
+                      <td className="py-2 px-3">{penjualan.salesman.namaSalesman}</td>
+                      <td className="text-sm py-2 px-3">
+                          {barangLaporan.map((brg, idx) => (
+                            <ul key={idx}>
+                              <li>{brg.barcodeBarang}</li>
+                            </ul>
+                          ))}
+                        </td>
                       <td className="py-2 px-3 flex items-center justify-center">
                         <div className="flex flex-row gap-3">
                           <IconButton size="md" color="light-blue">
@@ -109,10 +149,10 @@ function Notifikasi7Excelcom() {
                               color="orange"
                               onClick={() => {
                                 const phone = encodeURIComponent(
-                                  penjualan.noTelpCustomer
+                                  penjualan.customer.telp
                                 ); // Mengkodekan nomor telepon
                                 const message = encodeURIComponent(
-                                  `Selamat pagi kak ${penjualan.namaCustomer}%0APerkenalkan saya ${penjualan.namaSalesman} dari Excellent Computer Semarang%0ABagaimana kabarnya Kak? Semoga selalu dalam lindunganNya Aamiin`
+                                  `Selamat pagi kak ${penjualan.customer.nama_customer}%0APerkenalkan saya ${penjualan.salesman.namaSalesman} dari Excellent Computer Semarang ABagaimana kabarnya Kak? Semoga selalu dalam lindunganNya Aamiin`
                                 );
                                 window.open(
                                   `https://api.whatsapp.com/send?phone=${phone}&text=${message}`
@@ -125,7 +165,8 @@ function Notifikasi7Excelcom() {
                         </div>
                       </td>{" "}
                     </tr>
-                  ))
+                    )
+})
                 ) : (
                   <tr>
                     <td
@@ -143,7 +184,7 @@ function Notifikasi7Excelcom() {
 
         <main className="bg-white shadow-lg p-5 my-5 rounded">
           <Typography variant="lead" className="uppercase">
-            KONFIRMASI NOTIFIKASI 7 HARI EXCELCOM
+            KONFIRMASI NOTIFIKASI 30 HARI EXCELCOM
           </Typography>
 
           <div className="rounded my-5 overflow-auto">
@@ -155,7 +196,9 @@ function Notifikasi7Excelcom() {
               <thead className="bg-blue-500 text-white">
                 <tr>
                   <th className="py-2 px-3 font-semibold w-[4%]">No</th>
-                  <th className="py-2 px-3 font-semibold">Tanggal Konfirmasi</th>
+                  <th className="py-2 px-3 font-semibold">
+                    Tanggal Konfirmasi
+                  </th>
                   <th className="py-2 px-3 font-semibold">Nama Customer</th>
                   <th className="py-2 px-3 font-semibold">Nama Salesman</th>
                   <th className="py-2 px-3 font-semibold">Keterangan</th>
