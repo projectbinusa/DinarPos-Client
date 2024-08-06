4fcf843985315310c854c5ccf213a44912f93388
daniii71 <daniaja0171@gmail.com>
2024-08-06 09:36:54 +0700

    export laporan service

--- src/views/itc/laporan/LaporanService.js
+++ src/views/itc/laporan/LaporanService.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import SidebarAdmin from "../../../component/SidebarAdmin";
 import {
   Breadcrumbs,
@@ -8,6 +8,8 @@ import {
   Input,
   Typography,
 } from "@material-tailwind/react";
+import { API_LAPORAN_SERVICE_EXPORT } from "../../../utils/BaseUrl";
+import axios from "axios";
 import {
   ArrowPathIcon,
   CheckIcon,
@@ -16,14 +18,54 @@ import {
   MagnifyingGlassIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline";
-// import $ from "jquery";
 
 function LaporanService() {
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+
+  const handleStartDateChange = (e) => {
+    setStartDate(e.target.value);
+  };
+
+  const handleEndDateChange = (e) => {
+    setEndDate(e.target.value);
+  };
+
+  // EXPORT LAPORAN SERVICE
+  const exportLaporanService = async (e) => {
+    e.preventDefault();
+    if (!startDate || !endDate) {
+      alert("Tanggal Awal dan Tanggal Akhir harus diisi.");
+      return;
+    }
+    try {
+      const response = await axios.get(
+        `${API_LAPORAN_SERVICE_EXPORT}/export/laporanServiceAll?tanggal_akhir=${endDate}&tanggal_awal=${startDate}`,
+        {
+          headers: { "auth-tgh": `jwt ${localStorage.getItem("token")}` },
+          responseType: "blob",
+        }
+      );
+
+      // Handle the blob response and download the file
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", "Laporan_Service_Export.xlsx");
+      document.body.appendChild(link);
+      link.click();
+      link.parentNode.removeChild(link);
+      console.log("Export successful");
+    } catch (error) {
+      console.error("Error saat mengunduh file:", error);
+    }
+  };
+
   return (
     <section className="lg:flex font-poppins bg-gray-50 min-h-screen">
       <SidebarAdmin />
       <div className="lg:ml-[18rem] ml-0 pt-24 lg:pt-5 w-full px-5 overflow-x-auto">
-      <div className="flex flex-col items-start lg:flex-row lg:items-center lg:justify-between">
+        <div className="flex flex-col items-start lg:flex-row lg:items-center lg:justify-between">
           <Typography variant="lead" className="uppercase">
             Laporan Service
           </Typography>
@@ -51,7 +93,8 @@ function LaporanService() {
               label="Tanggal Awal"
               color="blue"
               type="date"
-            // onChange={(e) => setStartDate(e.target.value)}
+              value={startDate}
+              onChange={handleStartDateChange}
             />
           </div>
           <div className="w-full">
@@ -60,62 +103,93 @@ function LaporanService() {
               label="Tanggal Akhir"
               color="blue"
               type="date"
-            // onChange={(e) => setEndDate(e.target.value)}
+              value={endDate}
+              onChange={handleEndDateChange}
             />
           </div>
           <div className="w-full lg:w-auto flex justify-start items-center">
-            <Button
-              variant="gradient"
-              color="blue"
-              // onClick={filterTangggal}
-              size="md"
-            >
+            <Button variant="gradient" color="blue" size="md">
               <MagnifyingGlassIcon className="w-5 h-5" />
             </Button>
           </div>
         </div>
         <div className="grid lg:grid-cols-3 grid-cols-1 gap-4">
-          <Card className="p-0">
-            <CardBody className="bg-blue-100 flex justify-between items-center rounded-t">
+          <Card className="p-0 cursor-pointer">
+            <CardBody className="bg-blue-100 flex flex-col justify-center items-center rounded-t">
               <Cog6ToothIcon className="h-12 w-12 text-blue-900" />
-              <Typography variant="paragraph" className="text-blue-900">
-                Service Terbaru
+              <Typography
+                variant="paragraph"
+                className="text-blue-900 text-center"
+              >
+                Service All
               </Typography>
+              <Button
+                variant="filled"
+                color="blue"
+                className="mt-4"
+                onClick={exportLaporanService} // Ensure onClick is used instead of type="submit"
+              >
+                Export
+              </Button>
             </CardBody>
           </Card>
           <Card className="p-0">
-            <CardBody className="bg-green-100 flex justify-between items-center rounded-t">
+            <CardBody className="bg-green-100 flex flex-col justify-center items-center rounded-t">
               <CheckIcon className="h-12 w-12 text-green-900" />
-              <Typography variant="paragraph" className="text-green-900">
+              <Typography
+                variant="paragraph"
+                className="text-green-900 text-center"
+              >
                 Service Ready
               </Typography>
+              <Button variant="filled" color="green" className="mt-4">
+                Action
+              </Button>
             </CardBody>
           </Card>
           <Card className="p-0">
-            <CardBody className="bg-yellow-100 flex justify-between items-center rounded-t">
+            <CardBody className="bg-yellow-100 flex flex-col justify-center items-center rounded-t">
               <ArrowPathIcon className="h-12 w-12 text-yellow-900" />
-              <Typography variant="paragraph" className="text-yellow-900">
+              <Typography
+                variant="paragraph"
+                className="text-yellow-900 text-center"
+              >
                 Service Proses
               </Typography>
+              <Button variant="filled" color="yellow" className="mt-4">
+                Action
+              </Button>
             </CardBody>
           </Card>
         </div>
         <br />
         <div className="grid lg:grid-cols-3 grid-cols-1 gap-4">
           <Card className="p-0">
-            <CardBody className="bg-red-100 flex justify-between items-center rounded-t">
+            <CardBody className="bg-red-100 flex flex-col justify-center items-center rounded-t">
               <XMarkIcon className="h-12 w-12 text-red-900" />
-              <Typography variant="paragraph" className="text-red-900">
+              <Typography
+                variant="paragraph"
+                className="text-red-900 text-center"
+              >
                 Service Cancel
               </Typography>
+              <Button variant="filled" color="red" className="mt-4">
+                Action
+              </Button>
             </CardBody>
           </Card>
           <Card className="p-0">
-            <CardBody className="bg-blue-100 flex justify-between items-center rounded-t">
+            <CardBody className="bg-blue-100 flex flex-col justify-center items-center rounded-t">
               <CheckCircleIcon className="h-12 w-12 text-blue-900" />
-              <Typography variant="paragraph" className="text-blue-900">
+              <Typography
+                variant="paragraph"
+                className="text-blue-900 text-center"
+              >
                 Service Taken
               </Typography>
+              <Button variant="filled" color="blue" className="mt-4">
+                Action
+              </Button>
             </CardBody>
           </Card>
         </div>
@@ -124,4 +198,5 @@ function LaporanService() {
     </section>
   );
 }
+
 export default LaporanService;
